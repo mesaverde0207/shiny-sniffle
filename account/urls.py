d7007f6c17940fe93966ae17f2c8c0382c806a13
y_fukuda <colors92window@gmail.com>
2021-04-04 19:47:27 +0900

    feat: func. of password change

--- account/urls.py
+++ account/urls.py
@@ -7,4 +7,10 @@ urlpatterns = [
     path('login/', views.LoginView.as_view(), name='login'),
     path('logout/', views.LogoutView.as_view(), name='logout'),
     path('', account_views.dashboard, name='dashboard'),
+    path('password_change/',
+         views.PasswordChangeView.as_view(),
+         name='password_change'),
+    path('password_change/done/',
+         views.PasswordChangeDoneView.as_view(),
+         name='password_change_done'),
 ]
