5134ab40d83e7542e0853cbcca82c493dd11785c
y_fukuda <colors92window@gmail.com>
2021-04-07 00:30:08 +0900

    feat: func. of resetting a password

--- account/urls.py
+++ account/urls.py
@@ -1,16 +1,17 @@
-from django.urls import path
+from django.urls import path, include
 from django.contrib.auth import views
 from . import views as account_views
 
 urlpatterns = [
-    # path('login/', views.user_login, name='login')
-    path('login/', views.LoginView.as_view(), name='login'),
-    path('logout/', views.LogoutView.as_view(), name='logout'),
-    path('', account_views.dashboard, name='dashboard'),
-    path('password_change/',
-         views.PasswordChangeView.as_view(),
-         name='password_change'),
-    path('password_change/done/',
-         views.PasswordChangeDoneView.as_view(),
-         name='password_change_done'),
-]
+     # path('login/', views.user_login, name='login')
+     path('login/', views.LoginView.as_view(), name='login'),
+     path('logout/', views.LogoutView.as_view(), name='logout'),
+     path('', account_views.dashboard, name='dashboard'),
+     path('password_change/',
+          views.PasswordChangeView.as_view(),
+          name='password_change'),
+     path('password_change/done/',
+          views.PasswordChangeDoneView.as_view(),
+          name='password_change_done'),
+     path('', include('django.contrib.auth.urls')),
+     ]
